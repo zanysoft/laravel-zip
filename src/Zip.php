5debb95a582fb54783e2a3bdfdd7413a55be8186
Mubashar Ahmad <mubasharahmad_pk@hotmail.com>
2017-09-22 11:21:32 +0500

    Update Zip.php

--- src/Zip.php
+++ src/Zip.php
@@ -397,8 +397,14 @@ class Zip {
         }
 
         if ( !is_writable($destination) ) throw new Exception('Destination path not writable');
+        
+        if (!is_array($files) && !is_string($files) && !is_null($files)) {
+            throw new Exception('Invalid extractable files');
+        }
+        
+		$files = is_array($files) ? $files : is_string($files) ? [$files]: $files;
 
-        if ( is_array($files) && @sizeof($files) != 0 ) {
+        if (@sizeof($files) != 0 ) {
 
             $file_matrix = $files;
 
