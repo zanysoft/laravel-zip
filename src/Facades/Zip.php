07f680bb1a48da6b9e894a78b96cde472e0c2bea
Vano Devium <webdevium@gmail.com>
2023-11-10 12:39:46 +0200

    Create temporary archive

--- src/Facades/Zip.php
+++ src/Facades/Zip.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static \ZanySoft\Zip\Zip open($zip_file)
  * @method static \ZanySoft\Zip\Zip create($zip_file, $overwrite = false)
+ * @method static \ZanySoft\Zip\Zip createTemporary()
  * @method static \ZanySoft\Zip\Zip check($zip_file)
  */
 class Zip extends Facade
